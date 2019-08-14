3f93a89098e5657278e19a517aca0c99a6205613
Reed James <reedtjames1234@gmail.com>
2019-08-14 16:27:49 -0500

    MVP complete

--- data/seeds/001-cars.js
+++ data/seeds/001-cars.js
@@ -8,7 +8,9 @@ exports.seed = function(knex) {
         // { id: 1, colName: "rowValue1" },
         // { id: 2, colName: "rowValue2" },
         // { id: 3, colName: "rowValue3" }
-        { vin: "1GTG5BE37F1061816", make: "Saab" }
+        { vin: "1GTG5BE37F1061816", make: "Saab" },
+        { vin: "NM0AS8F7XE1154439", make: "Jensen" },
+        { vin: "JH4DC54836S696502", make: "Volkswagen" }
         // { "vin": "1GTG5BE37F1061816", "make": "Saab", "model": "9-7X", "transmission-type": null, "mileage": 17389.66, "title-status": "clean" }
       ]);
     });
